596a3afd8ccae1f2c884685614ebb6182e80a383
petrachkov <petrachkov@obninsk.top-academy.local>
2024-04-07 21:25:08 +0300

    fast api begin

--- client_tornado.py
+++ client_tornado.py
@@ -1,5 +1,6 @@
 import tornado.websocket
 import tornado.ioloop
+import json
 
 class WebSocketClient(tornado.websocket.WebSocketClientConnection):
     async def send_message(self, message):
@@ -7,9 +8,14 @@ class WebSocketClient(tornado.websocket.WebSocketClientConnection):
         
 
 async def connect_to_server():
-    url = "ws://213.226.126.185:5666/ws"
+    #url = "ws://213.226.126.185:5666/ws"
+    url = "wss://ws-api.binance.com:443/ws-api/v3"
     client = await tornado.websocket.websocket_connect(url)
-    await client.send_message("Сервак ты чтоли")
+    await client.write_message(json.loads("""{
+        \"id\": \"922bcc6e-9de8-440d-9e84-7c80933a8d0d\",
+        \"method\": \"ping\"}
+        """)
+        )
     response = await client.read_message()
     print(f"Received: {response}")
 
